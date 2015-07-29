1152e6998c9b5c7ad0cd4f2903887f38c6e0e412
eaward <graymaul@gmail.com>
2015-07-28 22:30:51 -0400

    removed the annotations foder and the bin

--- annotations/bootstrapper.ts
+++
@@ -1,5 +0,0 @@
-///<reference path="../typings/tsd.d.ts" />
-export function bootstrap(app: string, modules?: Array<string>){
-    angular.module(app, modules);
-    angular.bootstrap(document, [app]);
-}
