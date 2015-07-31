0ec929c94e07a571f1442b6efb8ef6c7d9627b20
eaward <graymaul@gmail.com>
2015-07-30 21:32:56 -0400

    Added beginings to implementing UI-Router

--- router.ts
+++ router.ts
@@ -1,6 +1,6 @@
 ///<reference path="../../typings/tsd.d.ts" />
 let ngModule:angular.IModule;
-ngModule = angular.module('component.ui.router', ['ngRoute']);
+ngModule = angular.module('component.ui.router', ['ui.router']);
 export function Router({url, config = {}}: {url:string; config?:any}){
     return function(target){
         ngModule.config(['$routeProvider', ($routeProvider) =>{
@@ -10,6 +10,47 @@ export function Router({url, config = {}}: {url:string; config?:any}){
                 .toLowerCase();
             config.template = '<' + selector + '></' + selector + '>';
             $routeProvider.when(url, config);
+        }]);
+    }
+}
+
+///TODO: Replace with above and rename to just Router
+export function ComponentRouter({url, config = {}}: {url:string; config?: {parent?: Function; defaultRoute?: string; params?: any}}){
+    return function(target: Function){
+        ngModule.config(['$stateProvider', '$urlRouterProvider', ($stateProvider, $urlRouterProvider) => {
+                
+            var selector = target.name
+                .replace(/\W+/g, '-')
+                .replace(/([a-z\d])([A-Z])/g, '$1-$2')
+                .toLowerCase();
+                
+            //capturning the parent's state name.
+            var parentState: string = "";
+            if(config.parent){
+                parentState = config.parent.name
+                                    .replace(/\W+/g, '-')
+                                    .replace(/([a-z\d])([A-Z])/g, '$1-$2')
+                                    .toLowerCase();
+            }
+            var template = '<' + selector + '></' + selector + '>';
+            
+            var state = selector + config.parent ? '.' + parentState : '';
+            
+            if(config.defaultRoute){
+                $urlRouterProvider.otherwise(config.defaultRoute);
+            }
+            
+            var stateConfig:any;
+            
+            stateConfig.url = url;
+            stateConfig.template = template;
+            
+            Object.getOwnPropertyNames(config.params).forEach((item) => {
+                    stateConfig[item] = config.params[item];
+            });
+            
+            $stateProvider
+                .state(state, stateConfig);
         }]);
     }
 }
