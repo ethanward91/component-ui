0d077206e9d7c1198f144e4281abcfbbaa03574f
eaward <graymaul@gmail.com>
2015-07-31 09:13:18 -0400

    Updated the definition file for the router.

--- router.d.ts
+++ router.d.ts
@@ -4,4 +4,6 @@ declare module 'component/router'{
 
 declare module router{
 	function Router({url, config}: {url:string; config?:any});
+	function ComponentRouter({url, config}:{url: string; config?: {parent?: any; defaultRoute?: string; params?: any;}});
 }
+
